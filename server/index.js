cc31a1f7acef850d59427e14ae574ec425bd2d84
hanfamilym1 <michael@hanfamily.com>
2018-09-07 09:48:39 -0600

    Finished DB setup TS: 9:48

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const massive = require('massive')
 
 app.use(bodyParser.json())
 
 const port = 4000
 
-app.listen(port,()=>{console.log(`the earth is round on ${port}`)})
+massive(process.env.CONNECTION_STRING).then(connection => {
+    app.set('db', connection)
+    app.listen(port, () => { console.log(`the earth is round on ${port}`) })
+}).catch((err) => { console.log(err) })
+
